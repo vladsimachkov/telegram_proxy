0ffb091c26440767f121f74186e97ed53f2a4c48
Vlad <vlad.simachkov@gmail.com>
2020-04-12 10:24:49 +0300

    Init

---
+++ app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+from flask import Flask
+from flask import request
+from flask import make_response
+import requests
+
+try:
+  from urllib.parse import urlparse
+except:
+  from urlparse import urlparse
+
+app = Flask(__name__)
+
+base_path = ''
+key_prefix = "/bot"
+telegram_api = "https://api.telegram.org"
+
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def home(path):
+    if not request.full_path.startswith(key_prefix):
+        return make_response("401 Unauthorized.", 401)
+    method = request.method
+    full_path = request.full_path
+    url = telegram_api + full_path
+    data = None if method.upper() == "GET" else request.form.to_dict()
+    req = requests.request(method, url, data=data)
+    return make_response(req.content, req.status_code)
